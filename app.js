88100246a92279db1b578b03dfae0655af4053c9
vvoinescu <Vlad.Voinescu@endava.com>
2023-01-06 10:48:52 +0200

    tic tac toe vanillaJs, wip: minor bugfix after starting a new game

---
+++ app.js
@@ -0,0 +1,60 @@
+let turn = true;
+let xindexes = [];
+let yIndexes = [];
+const winners = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const cell = document.getElementsByClassName("table-cell");
+
+for (let i = 0; i < cell.length; i++) {
+  cell[i].addEventListener(
+    "click",
+    function () {
+      if (!cell[i].innerHTML) {
+        cell[i].innerHTML = turn ? "X" : "O";
+        addValues(turn, i);
+        turn = !turn;
+      }
+    },
+    false
+  );
+}
+
+function addValues(turn, i) {
+  if (turn) {
+    xindexes.push(i);
+    check(xindexes, turn);
+  } else {
+    yIndexes.push(i);
+    check(yIndexes, turn);
+  }
+}
+
+function check(passedArray, turn) {
+  winners.map((winnersArray) => {
+    if (winnersArray.every((element) => passedArray.includes(element))) {
+      alert(`${turn ? "X" : "O"} won the game`);
+      clear();
+    }
+  });
+  if (xindexes.length + yIndexes.length === 9) {
+    alert(`Draw`);
+    clear();
+  }
+}
+
+function clear() {
+  turn = true;
+  xindexes = [];
+  yIndexes = [];
+  for (let i = 0; i < cell.length; i++) {
+    cell[i].innerHTML = "";
+  }
+}
